d958f6cb3ae2afb7429913f5f606682aba8fcdf7
Antal István Miklós <istvan.m.antal@gmail.com>
2014-05-26 12:05:34 +0100

    --setup option for deployPull

--- tasks/deployinator.js
+++ tasks/deployinator.js
@@ -8,61 +8,95 @@
 
 module.exports = function(grunt) {
     var deployinator = require('../lib/deployinator.js');
-    
+
+    function deploySetup(options) {
+        var pkg = grunt.file.readJSON('package.json');
+
+        options.package = pkg;
+
+        if (!pkg.repository ||
+            !pkg.repository.type ||
+            pkg.repository.type !== 'git' ||
+            !pkg.repository.url) {
+            grunt.fail.fatal('Please sepecify the repository in the package.json');
+        }
+
+        return deployinator.deploySetup(options);
+    }
+
     grunt.registerMultiTask('deployPull',
         'SSH into a server and do a pull', function() {
         var options = this.options();
-        options.increment = getVersionIncrement();
-        
+        var done = this.async();
+
         if (!options.host || !options.directory) {
             grunt.fail.fatal('Please sepecify a host and directory.');
         }
-        
-        var done = this.async();
-        
+
+        if (grunt.option('setup')) {
+            return deploySetup(options).then(function (output) {
+                grunt.log.writeln(output);
+                done();
+            });
+        }
+
+        options.increment = getVersionIncrement();
+
         deployinator.deployPull(options).then(function (output) {
             grunt.log.writeln(output);
             done();
         });
     });
-    
+
     grunt.registerMultiTask('deployPush',
         'Push to a server to deploy', function() {
         var options = this.options();
         options.increment = getVersionIncrement();
-        
+
         if (!options.remote) {
             grunt.fail.fatal('Please sepecify a remote.');
         }
-        
+
         var done = this.async();
-        
+
         deployinator.deployPush(options).then(function (output) {
             grunt.log.writeln(output);
             done();
         });
     });
-    
+
+    grunt.registerMultiTask('npmDeploy',
+        'Publish an NPM module', function() {
+
+        var done = this.async();
+
+        //TODO
+        deployinator.npmDeploy().then(function (output) {
+            grunt.log.writeln(output);
+            done();
+        });
+    });
+
     grunt.registerTask('tagRelease',
         'Creates a git tag for the release.', function() {
-        
+
         var done = this.async(),
             options = this.options();
-    
+
         options.increment = getVersionIncrement();
-        
+
         deployinator.tagRelease(options).then(done);
     });
-    
+
     function getVersionIncrement() {
         if (grunt.option('majorRelease')) {
             return 'major';
         }
-        
+
         if (grunt.option('release')) {
             return 'minor';
         }
-        
+
         return 'patch';
     }
 };
