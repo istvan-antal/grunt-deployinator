8c0f039ea5da06a18518cb31d0242cd3749dacb0
Antal István Miklós <istvan.m.antal@gmail.com>
2014-05-29 07:06:19 +0100

    Tests for command module

---
+++ spec/commandSpec.js
@@ -0,0 +1,20 @@
+var command = require('../lib/command.js');
+
+describe('Command utilities', function () {
+
+    it('should resolve for a successful command', function (done) {
+        command.run('pwd').then(function () {
+            done();
+        }, function () {
+            done(true);
+        });
+    });
+
+    it('should reject for an unsuccessful command', function (done) {
+        command.run('non0existing-command').then(function () {
+            done(true);
+        }, function () {
+            done();
+        });
+    });
+});
